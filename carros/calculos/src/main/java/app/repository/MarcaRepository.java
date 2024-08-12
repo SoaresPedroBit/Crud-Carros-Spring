d7664fc1f4dbea803050efbe3a1339a111acbb98
Pedro Soares <141785905+SoaresPedroBit@users.noreply.github.com>
2024-08-12 13:22:51 -0300

    Delete carros directory

--- carros/calculos/src/main/java/app/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Marca;
-
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-	
-}
